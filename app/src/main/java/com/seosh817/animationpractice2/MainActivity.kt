c9d00faa9402d3fb291d6bf1b330b72079ae83e8
seunghwan <slflfl12@naver.com>
2021-08-28 20:27:20 +0900

    bottom navigation setOnItemSelectedListener

--- app/src/main/java/com/seosh817/animationpractice2/MainActivity.kt
+++ app/src/main/java/com/seosh817/animationpractice2/MainActivity.kt
@@ -2,10 +2,38 @@ package com.seosh817.animationpractice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seosh817.animationpractice2.databinding.ActivityMainBinding
+import com.seosh817.animationpractice2.utils.FragmentNavigator
+import com.seosh817.animationpractice2.utils.viewBindings
 
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBindings(ActivityMainBinding::inflate)
+
+    private val navigator by lazy {
+        FragmentNavigator(supportFragmentManager, R.id.container)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.tab_home ->
+                    navigator.navigate(
+                        "home",
+                        R.anim.slide_in_left,
+                        R.anim.slide_out_right
+                    ) { HomeFragment() }
+                R.id.tab_settings ->
+                    navigator.navigate(
+                        "settings",
+                        R.anim.slide_in_left,
+                        R.anim.slide_out_right
+                    ) { SettingsFragment() }
+                else -> false
+                
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/seosh817/animationpractice2/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.seosh817.animationpractice2
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment: Fragment() {
+}
