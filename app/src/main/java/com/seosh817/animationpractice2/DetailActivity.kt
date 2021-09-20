1a2350f5673fe26f2aa1655c051d180bbb710335
seunghwan <slflfl12@naver.com>
2021-09-20 22:11:54 +0900

    DetailActivity (asld, avd 다시보기)

--- app/src/main/java/com/seosh817/animationpractice2/DetailActivity.kt
+++ app/src/main/java/com/seosh817/animationpractice2/DetailActivity.kt
@@ -1,6 +1,23 @@
 package com.seosh817.animationpractice2
 
+import android.os.Bundle
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.transition.platform.MaterialSharedAxis
+import com.seosh817.animationpractice2.databinding.ActivityDetailBinding
+import com.seosh817.animationpractice2.utils.viewBindings
 
 class DetailActivity: AppCompatActivity() {
+    private val binding by viewBindings(ActivityDetailBinding::inflate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+        window.enterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, true).apply {
+            addTarget(R.id.root)
+        }
+        window.allowEnterTransitionOverlap = true
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
 }
