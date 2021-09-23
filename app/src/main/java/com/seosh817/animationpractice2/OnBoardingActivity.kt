75ea1ae99fd54cdf1cc5f196792b714ddbc654ec
seunghwan <slflfl12@naver.com>
2021-09-23 20:29:13 +0900

    onboarding layout

--- app/src/main/java/com/seosh817/animationpractice2/OnBoardingActivity.kt
+++ app/src/main/java/com/seosh817/animationpractice2/OnBoardingActivity.kt
@@ -1,6 +1,25 @@
 package com.seosh817.animationpractice2
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.seosh817.animationpractice2.databinding.ActivityOnboardingBinding
+import com.seosh817.animationpractice2.utils.viewBindings
 
 class OnBoardingActivity: AppCompatActivity() {
+    private val binding by viewBindings(ActivityOnboardingBinding::inflate)
+
+    private val callback = object: ViewPager2.OnPageChangeCallback() {
+
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 }
